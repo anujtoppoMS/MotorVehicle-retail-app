7e07653299788306a0ba3862df522c8f08b1fd0b
anuj.toppo <anuj.toppo@outlook.com>
2024-11-09 13:40:38 +0530

    updated deleted

--- BillingSystem/Sales/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.stocks, name='stocks'),
-    path('customer', views.customer_reg, name='customer'),
-    path('billings_process', views.billings_process, name='billings_process'),
-    path('generate_Invoice', views.generate_Invoice, name='generate_Invoice'),
-]
--- BillingSystem/frontend/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class frontendConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'frontend'
