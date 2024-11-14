cf6d0d4b2473a0d5fd10a029a79d0a28a5d23e57
anuj.toppo <anuj.toppo@outlook.com>
2024-11-15 00:20:32 +0530

    updated the entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 /opt/bin/python manage.py migrate
 
 # uwsgi --socket :8000 --master --enable-threads --module BillingSystem.wsgi:application
-uwsgi --socket :8000 --master --enable-threads --buffer-size 32768 --module BillingSystem.wsgi:application
+uwsgi --socket :8020 --master --enable-threads --buffer-size 32768 --module BillingSystem.wsgi:application
