c0f8519eb4505297b574155464f769b3cd772ff3
anuj.toppo <anuj.toppo@outlook.com>
2024-11-26 20:05:52 +0530

    modified scripts for the debug

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -e
 # APP_PORT=${PORT:-8000}
+# /scripts/entrypoint.sh 
+echo "Current working directory: $(pwd)" 
+ls -l
 cd /BillingSystem
 /opt/bin/python manage.py migrate --noinput
+echo "Current working directory: $(pwd)" 
+ls -l
 # uwsgi --socket :8000 --master --enable-threads --module BillingSystem.wsgi:application
 # /opt/bin/uwsgi --http :8000 --master --enable-threads --buffer-size 32768 --module BillingSystem.wsgi:application
 # /opt/bin/gunicorn --worker-tmp-dir /dev/shm BillingSystem.wsgi:application --timeout 120 --log-level=debug --bind "0.0.0.0:${APP_PORT}"
