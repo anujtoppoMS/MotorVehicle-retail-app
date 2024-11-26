c15b098266a726a99ebf9a9399c41b78328d0557
anuj.toppo <anuj.toppo@outlook.com>
2024-11-26 12:43:14 +0530

    update entrypoint script

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -e
-APP_PORT=${PORT:-8000}
-/opt/bin/python manage.py migrate
-
+# APP_PORT=${PORT:-8000}
 cd /BillingSystem
+/opt/bin/python manage.py migrate
 # uwsgi --socket :8000 --master --enable-threads --module BillingSystem.wsgi:application
 # /opt/bin/uwsgi --http :8000 --master --enable-threads --buffer-size 32768 --module BillingSystem.wsgi:application
 # /opt/bin/gunicorn --worker-tmp-dir /dev/shm BillingSystem.wsgi:application --timeout 120 --log-level=debug --bind "0.0.0.0:${APP_PORT}"
